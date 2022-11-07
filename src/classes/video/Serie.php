bfd7812d6caa1be7baa231fe1039c803fa8f50b2
sonii667 <sonij590833@gmail.com>
2022-11-07 14:30:14 +0100

    Classe série et épisode

--- src/classes/video/Serie.php
+++ src/classes/video/Serie.php
@@ -5,27 +5,37 @@ namespace iutnc\netvod\video;
 class Serie
 {
 
-    protected array $saison;
-    protected int $nbsaison;
-    protected string $genre,$type;
+    protected array $listEpisode;
+    protected string $titre, $genre, $publiqueVise, $descriptif, $sortie, $dateAjout;
+    protected int $nmbEpisode;
 
     /**
-     * @param int $nbsaison
+     * @param array $listEpisode
+     * @param string $titre
      * @param string $genre
-     * @param string $type
+     * @param string $publiqueVise
+     * @param string $descriptif
+     * @param string $sortie
+     * @param string $dateAjout
+     * @param int $nmbEpisode
      */
-    public function __construct(int $nbsaison, string $genre, string $type)
+    public function __construct(array $listEpisode = [], string $titre, string $genre, string $publiqueVise, string $descriptif, string $sortie)
     {
-        $this->saison = [];
-        $this->nbsaison = $nbsaison;
+        $this->listEpisode = $listEpisode;
+        $this->titre = $titre;
         $this->genre = $genre;
-        $this->type = $type;
+        $this->publiqueVise = $publiqueVise;
+        $this->descriptif = $descriptif;
+        $this->sortie = $sortie;
+        $this->dateAjout = date('y-m-j');
+        $this->nmbEpisode = count($listEpisode);
     }
 
-    public function ajouterSaison(Saison $d):void{
-        array_push($this->saison,$d);
-    }
 
+    public function ajouterEpisode(Episode $d): void
+    {
+        array_push($this->saison, $d);
+    }
 
 
 }
