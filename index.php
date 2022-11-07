b02770abf9a9cb04f78d8534990a36d22cfc2f4e
lucasbcap <beal.lucas2002@gmail.com>
2022-11-07 11:15:02 +0100

    Creation de VideoRender.php

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ session_start();
 
 ConnectionFactory::setConfig("config.ini");
 
+$v = new \iutnc\netvod\vidéo\Video("test","test","test","test",0,0);
+$vRender = new \iutnc\netvod\Render\VideoRender($v);
+
+echo $vRender->render();
 
 $html = new Dispatcher();
 $html->run();
---
+++ src/classes/Render/Render.php
@@ -0,0 +1,8 @@
+<?php
+namespace iutnc\netvod\Render;
+abstract class Render
+{
+
+    abstract public function render():string;
+
+}
