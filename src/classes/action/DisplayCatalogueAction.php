a891088d6674e20e32f91561480ded54cc801ea2
Juliette Raoult <julietteraoult54@gmail.com>
2022-11-07 14:36:29 +0100

    debut affichage catalogue

--- src/classes/action/DisplayCatalogueAction.php
+++ src/classes/action/DisplayCatalogueAction.php
@@ -2,6 +2,8 @@
 
 namespace iutnc\netvod\action;
 
+use iutnc\netvod\db\ConnectionFactory;
+
 class DisplayCatalogueAction extends \iutnc\netvod\action\Action
 {
 
@@ -13,7 +15,12 @@ class DisplayCatalogueAction extends \iutnc\netvod\action\Action
     public function execute(): string
     {
         $res="<h1>Catalogue : </h1> ";
-
+        $bdd = ConnectionFactory::makeConnection();
+        $c1 = $bdd->query("SELECT titre from serie");
+        $c1->execute();
+        while($data  = $c1->fetch()){
+            $res .= $data['titre'] . "\n";
+        }
 
         return $res;
     }
